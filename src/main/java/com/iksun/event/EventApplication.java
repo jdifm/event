c183e007f47d3a6f043aa29654db4d863dfee008
sunchoi <ischoi07@gmail.com>
2019-03-28 01:30:38 +0900

    TEST

--- src/main/java/com/iksun/event/EventApplication.java
+++ src/main/java/com/iksun/event/EventApplication.java
@@ -8,7 +8,6 @@ public class EventApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
-        System.out.print("TEST");
     }
 
 }
